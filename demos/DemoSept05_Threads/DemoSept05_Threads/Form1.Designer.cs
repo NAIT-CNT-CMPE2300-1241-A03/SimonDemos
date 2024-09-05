6f9b8f16478ce8e61877fdad8b2df9233f82c63d
Simon Walker <simonw@nait.ca>
2024-09-05 09:21:02 -0600

    Demo on image manip, up to colour counting

---
+++ demos/DemoSept05_Threads/DemoSept05_Threads/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace DemoSept05_Threads
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UI_PB_Image = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.UI_PB_Image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UI_PB_Image
+            // 
+            this.UI_PB_Image.Location = new System.Drawing.Point(12, 12);
+            this.UI_PB_Image.Name = "UI_PB_Image";
+            this.UI_PB_Image.Size = new System.Drawing.Size(504, 426);
+            this.UI_PB_Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.UI_PB_Image.TabIndex = 0;
+            this.UI_PB_Image.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UI_PB_Image);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.UI_PB_Image)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox UI_PB_Image;
+    }
+}
+
---
+++ demos/DemoSept05_Threads/DemoSept05_Threads/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoSept05_Threads
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string fname = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();
+
+            Text = fname;
+
+            try
+            {
+                Bitmap bm = (Bitmap)Bitmap.FromFile(fname);
+                Console.WriteLine($"X:{bm.Width} Y:{bm.Height}");
+
+                UI_PB_Image.Image = bm;
+
+                Process(bm);
+            }
+            catch (Exception err)
+            {
+                System.Diagnostics.Trace.WriteLine($"DragDrop : {err.Message}");
+
+                // nice message to user
+                MessageBox.Show("You did not appear to drop an image! Please try again!");
+
+                return;
+            }
+
+        }
+
+        private static void Process(Bitmap bm)
+        {
+            int iCount = 0;
+            for (int y = 0; y < bm.Height; y++)
+            {
+                for (int x = 0; x < bm.Width; x++)
+                {
+                    Color c = bm.GetPixel(x, y);
+                                        
+                    if (c.R+c.G+c.B < 128)
+                    {
+                        iCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine ($"There are {iCount} not so bright pixels!");
+        }
+    }
+}
