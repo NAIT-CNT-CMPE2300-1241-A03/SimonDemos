1d125b359bd74bb00aaf80d4adb2b355e19a1e5c
Simon Walker <simonw@nait.ca>
2024-10-17 09:21:36 -0600

    new demo

---
+++ demos/demo_oct_17_2024_collections/demo_oct_17_2024_collections/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_oct_17_2024_collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello! I have vowels";
+
+            //foreach (char c in input.Except(
+            //    new char[] { 
+            //        'a', 'e', 'i', 'o', 'u', 
+            //        'A', 'E', 'I', 'O', 'U' 
+            //    }))
+            string s = new string (
+                input.Except(
+                new char[] {
+                    'a', 'e', 'i', 'o', 'u',
+                    'A', 'E', 'I', 'O', 'U'
+                }).ToArray());
+
+
+                
+            Console.WriteLine(s);
+
+            Console.ReadKey();
+
+            // what if:
+            // I want this as a string?
+            // I want this to be case insensitive?
+
+        }
+    }
+}
